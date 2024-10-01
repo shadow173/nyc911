4e7180a9102cec332753219254cd44d84c6a91a1
shadow173 <40893913+shadow173@users.noreply.github.com>
2024-09-30 23:28:46 -0400

    Initial commit

---
+++ backend/drizzle/0001_heavy_ted_forrester.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "incidents" ADD COLUMN "coordinates" "point" NOT NULL;
---
+++ backend/src/services/createAccount.ts
@@ -0,0 +1,8 @@
+
+
+    export const createAccount(email: string, name: string, password: string): Promise<Account> {
+
+    };
+
+
+    
