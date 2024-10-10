5e59b248a3ac407021035774dd791389b72f89e8
shadow173 <40893913+shadow173@users.noreply.github.com>
2024-10-10 16:02:44 -0400

    Update addNoteUpdate.ts

--- backend/src/controllers/addNoteUpdate.ts
+++ backend/src/controllers/addNoteUpdate.ts
@@ -1,16 +1,12 @@
-import { addNoteToIncident } from "../services/addNoteToIncident"
+import { addNoteToIncident } from "../services/addNoteToIncident";
 
-
-
-export const addNoteUpdate = async ({body, cookie}:any): Promise<object> => {
-// add authentication before all this!!
-// this is a very basic implementation
-    const { unitId, noteDescription } = body
-
-   const addedNote = await addNoteToIncident(null, noteDescription, unitId)
-
-   return {
+export const addNoteUpdate = async ({ body, cookie }: any): Promise<object> => {
+  // add authentication before all this!!
+  // this is a very basic implementation
+  const { unitId, noteDescription } = body;
+  const addedNote = await addNoteToIncident(null, noteDescription, unitId);
+  return {
     message: "Note added Successfully",
-    addedNote: addedNote
-   }
-}
+    addedNote: addedNote,
+  };
+};
