9e46d760d94e014f9793dba1a7e1f5ee85de1e91
shadow173 <40893913+shadow173@users.noreply.github.com>
2024-10-11 15:20:18 -0400

    Update registerUser.ts

--- backend/src/controllers/registerUser.ts
+++ backend/src/controllers/registerUser.ts
@@ -5,6 +5,7 @@ import { db } from '../utils/db.ts';
 import { hashPassword } from '../utils/encryptPassword.ts';
 import { eq } from 'drizzle-orm'
 import { normalizeEmail } from '../services/normalizeEmail.ts';
+import { signToken } from '../utils/jwt.ts';
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 
@@ -18,13 +19,22 @@ export const registerUser = async ({ body, cookie }: any) => {
     }
     const hashedPassword = await hashPassword(password)
     const correctEmail = normalizeEmail(email)
-    // hash password
 
     // create user
 // with isEmailVerified = false.
+    const user = await db.insert(users).values({
+        email: email,
+        isAdmin: false,
+        password: hashedPassword,
+        isEmailVerified: false,
+    }).returning()
 
     // set cookie
+    const token =  signToken({email: email, id: user[0].id, isActive: 'false', isEmailVerified: false, isAgencyEmailVerified:  false, needsManualApproval: false})
     
+    // set cookie as token
+    
+    // redirect user to 
     // generate email token, expires in 12 hours
 
     // send verification email
